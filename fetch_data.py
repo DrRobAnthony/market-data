2deea3e4dadcdf5f799bd5f11ecddce8091549c7
DrRobAnthony <rob@drrobanthony.com>
2026-08-23 13:46:09 -0500

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -41,7 +41,7 @@ KRAKEN_PAIRS = {"BTC": "XBTUSD", "ETH": "ETHUSD", "SOL": "SOLUSD",
 
 PARAMS = {"ema_fast": 50, "ema_slow": 200, "atr_n": 14, "breakout_n": 55,
           "sl_atr": 3.0, "tp_atr": 7.5, "rsi_max_long": 80,
-          "rsi_min_short": 20}
+          "rsi_min_short": 999}
 
 
 # ---------------- data fetching ----------------
